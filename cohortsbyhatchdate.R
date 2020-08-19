ccbaa42299f1a03b1e7f1fafbf32b2684bd85ddd
Tara Dolan <dolan.tara.e@gmail.com>
2020-08-19 12:03:24 -0400

    created the plot for Mattituck where the extracted fish are overlaid over the distribution of all the fish caught that week

--- cohortsbyhatchdate.R
+++ cohortsbyhatchdate.R
@@ -15,6 +15,7 @@ library('ggrepel')
 
 mtage <-read.csv(file="mtcohortreassignAug182020.csv", header=TRUE)
 mtage <-mutate(mtage, bday=as.Date(hatch.date), cohort=as.factor(new.cohort)) %>%slice(1:49)
+allmelt <-read.csv(file="allmeltdec2019.csv", header=TRUE)
 
 mtage %>%
   #ggplot(aes(x=bday, y=n_distinct(samp)))+ #turn off cohort sorting
@@ -36,4 +37,51 @@ mtage %>%
 #ggsave("Mtcohortassign.png", path="/Users/tdolan/Documents/WIP research/microsats/microsat_figs")
 #dev.off()
 
-#Ridges graphs for Mattituck
+#histograms where catherine's cohorts are overlaid on the regular cohort data. 
+## original script is called "field graphs 9_10_19.R"
+mtage <-dplyr::select(mtage,-cohort,-old.pop)%>%mutate(source="cc")
+
+mtmelt <-filter(allmelt, Bay=="Mattituck") %>%dplyr::rename(catch.date=Date, Location=Bay, TL=value) %>%mutate(source="tows")
+weeks <-dplyr::select(mtmelt,catch.date,Week)
+mtage <-left_join(mtage,weeks)
+mtfnew <-full_join(mtage, mtmelt, by=c("catch.date","TL","Location","Year","source","Week")) %>% mutate(Year=as.factor(Year),catch.date=as.Date(catch.date))
+
+split <-c(40,	47,	49,	43,	57,	70)
+splits15 <- as.data.frame(split) %>% mutate(Year="2015")
+splits15$catch.date <- as.Date(c("2015-06-24",	"2015-07-07",	"2015-07-21",	"2015-08-03",	"2015-08-17",	"2015-09-01"))
+split <-c(49,	52,	63,	84)
+splits16 <- as.data.frame(split) %>% mutate(Year="2016")
+splits16$catch.date <- as.Date(c("2016-06-30",	"2016-07-11",	"2016-07-26",	"2016-08-08"))
+splits <-bind_rows(splits15,splits16) %>% mutate(Year=as.factor(Year),catch.date=as.Date(catch.date))
+
+mtfnew <-full_join(mtfnew, splits, by=c("Year","catch.date"))
+mtfnew <-unique(mtfnew) %>% mutate(day = format(as.Date(catch.date), format="%m/%d"))
+
+#both years in same plot
+ggplot(mtfnew,aes(x=TL)) + 
+  geom_histogram(data = subset(mtfnew,source=="cc"& new.cohort=="1"), fill="#e66101",color="black", position="identity",   show.legend=TRUE)+ 
+  geom_histogram(data = subset(mtfnew,source=="cc"& new.cohort=="2"), fill="#018571",color="black",position="identity",   show.legend=TRUE)+
+  geom_histogram(data = subset(mtfnew,source=="tows"), fill="#5e3c99",color="#5e3c99",position="identity",   alpha = 0.2, show.legend=TRUE)+
+  geom_vline(aes(xintercept=split), color="black", linetype="dashed", size=0.5) +
+  facet_grid(day~Year,scales="free_y")+
+  theme_classic()+
+  labs(x = "length (mm)",
+       title = "YOY cohorts by length",
+       subtitle = "Mattituck") 
+
+#separate plots for each year - Remember to change out year!
+mtfnew2 <-filter(mtfnew, Year=="2015")
+ggplot(mtfnew2,aes(x=TL)) + 
+  geom_histogram(data = subset(mtfnew2,source=="cc"& new.cohort=="1"), fill="#e66101",color="black", position="identity",   show.legend=TRUE)+ 
+  geom_histogram(data = subset(mtfnew2,source=="cc"& new.cohort=="2"), fill="#018571",color="black",position="identity",   show.legend=TRUE)+
+  geom_histogram(data = subset(mtfnew2,source=="tows"), fill="#5e3c99",color="#5e3c99",position="identity",   alpha = 0.2, show.legend=TRUE)+
+  geom_vline(aes(xintercept=split), color="black", linetype="dashed", size=0.5) +
+  facet_wrap(~day,scales="free_y",nrow=6)+
+  theme_classic()+
+  labs(x = "length (mm)",
+       title = "YOY cohorts by length",
+       subtitle = "Mattituck 2015") 
+
+
+##Shinnecock!
+
